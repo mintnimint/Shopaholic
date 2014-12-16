f885455cff639ae323fd11b2b443a028a6115d85
itarnnn <s5070077@kmitl.ac.th>
2014-12-17 00:13:50 +0700

    add Fn(Edit)

--- Shopaholic.py
+++ Shopaholic.py
@@ -11,19 +11,11 @@ class App:
    value = 0
    total = 0
    
-<<<<<<< HEAD
-   
    def __init__(self,master):
       master.minsize(width=200, height=250)
 
-=======
-   def __init__(self,master):
-      master.minsize(width=200, height=250)
-
-
->>>>>>> origin/master
       B_NEW = Tkinter.Button(master, text ="NEW", width=30, height=3, font=100, command = self.addTask)
-      B_EDIT = Tkinter.Button(master, text ="EDIT", width=30, height=3, font=100, command = self.Delete)
+      B_EDIT = Tkinter.Button(master, text ="EDIT", width=30, height=3, font=100, command = self.editTask)
       B_RESULT = Tkinter.Button(master, text ="CHECK TOTAL", width=30, font=100, height=3, command = self.showList)
       B_EXIT = Tkinter.Button(master, text ="EXIT", width=30, height=3, font=100, command = master.destroy)
 
@@ -62,13 +54,87 @@ class App:
       self.paylist.append(float(value))
       self.translist.append([float(value),self.e1.get(),self.e2.get(),self.e3.get()])
       tkMessageBox.showinfo( "Total Pay!" , str(value) + " THB\n" +"Total pay : "+str(sum(self.paylist))+" THB\n"+"Now you got "+ str(len(self.paylist))+" Order!")
-<<<<<<< HEAD
       
    def showList(self):
       tkMessageBox.showinfo( "Total Pay!" , str(sum(self.paylist)) + " THB\n" + "Your Pay List : " + str(self.paylist) )
 
-=======
->>>>>>> origin/master
+  
+   def editTask(self):
+      def whichSelected () :
+            print "At %s of %d" % (select.curselection(), len(self.translist))
+            return int(select.curselection()[0])
+
+      def deleteEntry() :
+            del self.translist[whichSelected()]
+            del self.paylist[whichSelected()]
+
+            setSelect ()
+
+      def loadEntry  () :
+            total, price, discount, quantity = self.translist[whichSelected()]
+            totalVar.set(total)
+            priceVar.set(price)
+            discountVar.set(discount)
+            quantityVar.set(quantity)
+            
+      def makeWindow () :
+            global totalVar, priceVar, discountVar,  quantityVar, select
+            win = Tk()
+
+            frame1 = Frame(win)
+            frame1.pack()
+            
+            Label(frame1, text="Price").grid(row=0, column=0, sticky=W)
+            priceVar = StringVar()
+            price = Entry(frame1, textvariable=priceVar)
+            price.grid(row=0, column=1, sticky=W)
+
+            Label(frame1, text="Discount").grid(row=1, column=0, sticky=W)
+            discountVar= StringVar()
+            discount= Entry(frame1, textvariable=discountVar)
+            discount.grid(row=1, column=1, sticky=W)
+
+            Label(frame1, text="Quantity").grid(row=2, column=0, sticky=W)
+            quantityVar= StringVar()
+            quantity= Entry(frame1, textvariable=quantityVar)
+            quantity.grid(row=2, column=1, sticky=W)
+
+            Label(frame1, text="Total").grid(row=3, column=0, sticky=W)
+            totalVar= StringVar()
+            total= Entry(frame1, textvariable=totalVar, state = DISABLED)
+            total.grid(row=3, column=1, sticky=W)
+
+            frame2 = Frame(win)       
+            frame2.pack()
+            b3 = Button(frame2,text="Delete",command=deleteEntry)
+            b4 = Button(frame2,text=" Load ",command=loadEntry)
+            b3.pack(side=LEFT); b4.pack(side=LEFT)
+
+            frame3 = Frame(win)      
+            frame3.pack()
+            scroll = Scrollbar(frame3, orient=VERTICAL)
+            select = Listbox(frame3, yscrollcommand=scroll.set, height=8)
+            scroll.config (command=select.yview)
+            scroll.pack(side=RIGHT, fill=Y)
+            select.pack(side=LEFT,  fill=BOTH, expand=1)
+            return win
+
+
+      def setSelect () :
+            self.translist.sort()
+            select.delete(0,END)
+            for element in self.translist :
+               select.insert (END, element)
+
+            self.paylist.sort()
+            select.delete(0,END)
+            for price in self.paylist :
+               select.insert (END, price)
+
+
+      win = makeWindow() 
+      setSelect ()
+      win.mainloop()
 
 window = App(master)
 
